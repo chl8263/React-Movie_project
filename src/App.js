f305a0cad7d755a61dda7a630af7e0ca443e59bb
wongyun <chl8263@naver.com>
2019-02-07 00:11:25 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
+import { async } from 'q';
 
 
 
@@ -17,42 +18,62 @@ class App extends Component {
   }
   
   componentDidMount(){
-    setTimeout( () => {
-      this.setState({
-        movies  : [
-          {
-            title:"극한직업",
-            poster :  "https://blogfiles.pstatic.net/MjAxOTAxMjJfMjgw/MDAxNTQ4MTE1MzY3MTc2.NWp5w1q7f3xN30YUV9rYtUqwq_zzjFdG6p-bHX4VSF0g.KOZCAuCFzneC5aN-pdtDyEVhu7cCieNLwVReE6OwQ2wg.JPEG.ssk7989/NaverBlog_20190122_090246_00.jpg",
+    this._getMovie();
+    // setTimeout( () => {
+    //   this.setState({
+    //     movies  : [
+    //       {
+    //         title:"극한직업",
+    //         poster :  "https://blogfiles.pstatic.net/MjAxOTAxMjJfMjgw/MDAxNTQ4MTE1MzY3MTc2.NWp5w1q7f3xN30YUV9rYtUqwq_zzjFdG6p-bHX4VSF0g.KOZCAuCFzneC5aN-pdtDyEVhu7cCieNLwVReE6OwQ2wg.JPEG.ssk7989/NaverBlog_20190122_090246_00.jpg",
         
-          },
-          {
-            title:"말모이",
-            poster :    "https://pds.joins.com/news/component/htmlphoto_mmdata/201811/27/htm_2018112772324365087.jpg",
-          },
+    //       },
+    //       {
+    //         title:"말모이",
+    //         poster :    "https://pds.joins.com/news/component/htmlphoto_mmdata/201811/27/htm_2018112772324365087.jpg",
+    //       },
         
-          {
-            title : "변호인",
-            poster :      "http://imgnews.naver.net/image/022/2013/12/23/20131223020394_0_59_20131223150906.jpg",
+    //       {
+    //         title : "변호인",
+    //         poster :      "http://imgnews.naver.net/image/022/2013/12/23/20131223020394_0_59_20131223150906.jpg",
         
-          },
-          {
-            title : "써니",
-            poster :      "http://blogfiles.naver.net/20110509_168/serenitatis_13049086331115LJcz_JPEG/76016_P11_175820.jpg"
+    //       },
+    //       {
+    //         title : "써니",
+    //         poster :      "http://blogfiles.naver.net/20110509_168/serenitatis_13049086331115LJcz_JPEG/76016_P11_175820.jpg"
         
-          },
-          {
-            title : "나는 전설이다",
-            poster : "https://postfiles.pstatic.net/20141128_276/gip2120_1417159857502G45KL_PNG/capture-20141128-161702.png?type=w2"
-          }
-        ]
-      })
-    }, 1000)
+    //       },
+    //       {
+    //         title : "나는 전설이다",
+    //         poster : "https://postfiles.pstatic.net/20141128_276/gip2120_1417159857502G45KL_PNG/capture-20141128-161702.png?type=w2"
+    //       }
+    //     ]
+    //   })
+    // }, 1000)
   }
 
+   _getMovie = async () =>  {
+    const movies  = await this._callApi();
+    console.log({movies})
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(error => console(error))
+  }
 
   _renderMovie = () => {
     const movies = this.state.movies.map( (movie , index )=> {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+      return <Movie title={movie.title} 
+        poster={movie.medium_cover_image} 
+        key={index} 
+        genres={movie.genres}
+        synopsis = {movie.synopsis}
+        />
     })
     return movies
   }
--- src/Movie.js
+++ src/Movie.js
@@ -2,36 +2,82 @@ import React, { Component } from 'react';
 import './Movie.css';
 import propTypes from 'prop-types';
 
-class Movie extends Component{
+// class Movie extends Component{
 
-    static propTypes = {
-        title : propTypes.string.isRequired,
-        poster : propTypes.string.isRequired
-    }
+//     static propTypes = {
+//         title : propTypes.string.isRequired,
+//         poster : propTypes.string.isRequired
+//     }
 
-    render(){
+//     render(){
         
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster}/>
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         )
+//     }
+// }
+
+function Movie ({title , poster,genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className = "Movie_Columns">
+                <MoviePoster poster={poster} alt={title}/>
             </div>
-        )
-    }
+            <div className = "Movie_Columns">
+                <h1>{title}</h1>
+                <div className="Movie_Genre">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+            </div>
+            <p className="Movie_Synopsis">
+                {synopsis}
+            </p>
+        </div>
+    ) 
 }
 
-class MoviePoster extends Component{
+// class MoviePoster extends Component{
 
-    static propTypes = {
-        poster : propTypes.string.isRequired
-    }
+//     static propTypes = {
+//         poster : propTypes.string.isRequired
+//     }
 
 
-    render(){
-        return (
-            <img src = {this.props.poster}/>
-        )
-    }
+//     render(){
+//         return (
+//             <img src = {this.props.poster}/>
+//         )
+//     }
+// }
+
+function MoviePoster({poster,alt}){
+    return (
+        <img src = {poster} alt="Movie Poster" title={alt} className="Movie_Poster"/>
+    )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie_Genre">{genre}</span>
+    )
+}
+
+Movie.propTypes = {
+    title : propTypes.string.isRequired,
+    poster : propTypes.string.isRequired,
+    generes : propTypes.string.isRequired,
+    sysnopsis : propTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster : propTypes.string.isRequired,
+    alt : propTypes.string.isRequired
+}
+MovieGenre.propTypes = {
+    generes : propTypes.string.isRequired
 }
 
 export default Movie
